cf4edac85795961bc6abd325c24c2537ac9d810f
betmma <betmma@users.noreply.github.com>
2026-08-25 21:53:10 +0800

    remake trick spell

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-VERSION="0.4.7.5"
+VERSION="0.4.7.6"
 WINDOW_WIDTH,WINDOW_HEIGHT=love.graphics.getDimensions()
 GAME_NAME="Glossy Quagmire"
 IS_WEB=type(jit)~="table"
--- stages/stage4/spellcards/trick.lua
+++ stages/stage4/spellcards/trick.lua
@@ -14,80 +14,81 @@ return BossManager.SpellcardPhase{
         local posp=copyTable(G.runInfo.player.kinematicState.pos)
         Portal.setOuterPortals(posp)
         local dir0=G.runInfo.player.viewDirection
-        local posb=geo:rThetaGo(posp,330,dir0-math.pi/2)
-        Portal.setOuterPortals(posb)
+        local posb=geo:rThetaGo(posp,200,dir0-math.pi/2+0.3)
+        local outerPortals=Portal.setOuterPortals(posb)
+        outerPortals[1]:remove() -- remove top and bottom
+        outerPortals[3]:remove()
+        for i=1,4 do
+            outerPortals[i]=nil
+        end
         DanmakuFuncs.moveToInTime(boss,posb,40)
         wait(40)
         Portal.zoomC=80
-        local bigPortalSentry=DanmakuFuncs.PortalOnSentry(posb,dir0+math.pi/2,370,150,false,60,{draw=true,width=3,range=30})
+        local bigPortalSentry=DanmakuFuncs.PortalOnSentry(posb,dir0+math.pi/2,370,-150,false,60,{draw=true,width=3,range=300})
         local bigPortal=bigPortalSentry.any.portal
-        local smallPortalAngle=function(dt)
-            local t=bigPortalSentry.frame+(dt or 0)
-            return dir0+math.pi/2+math.sin(t/200)*math.pi/3
+        local smallPortalAngle=dir0+math.pi/2
+        local smallLength=200
+        local smallDistanceFunc=function ()
+            local final=200
+            return final+(350-final)*math.exp(-bigPortalSentry.frame/30)
         end
-        local smallLength=50
-        local smallDistance=100
-        local smallPortalSentry=DanmakuFuncs.PortalOnSentry(posb,dir0+math.pi/2,smallLength,-smallDistance,true,60,{draw=true,width=3,range=30})
+        local smallPortalSentry=DanmakuFuncs.PortalOnSentry(posb,dir0-math.pi/2,smallLength,-smallDistanceFunc(),true,60,{draw=true,width=3,range=300})
+        Event.LoopEvent{obj=smallPortalSentry,period=1,executeFunc=function ()
+            smallPortalSentry.any.width=-smallDistanceFunc()
+        end}
+        --- flames to prevent escape
+        for x=-350,350,10 do
+            local xabs=math.abs(x)
+            local xRatio=(xabs-smallLength)/(350-smallLength)
+            if xabs<smallLength-10 then
+                goto continue
+            elseif xabs<smallLength+10 then
+                x=math.sign(x)*(smallLength+10)
+            end
+            local p0=geo:rThetaGoRef(posb,x,dir0)
+            local p1,dir1=geo:rThetaGoRef(p0,-350,dir0-math.pi/2)
+            local bullet=Bullet{kinematicState={pos=p1,dir=dir1,speed=0,skipPortal=true},lifeFrame=9999,sprite=BulletSprites.flame.black,extraUpdate={Action.FadeIn(20,false),function (self)
+                self.kinematicState.pos=geo:rThetaGoRef(p0,-smallDistanceFunc()*(1-xRatio),dir0-math.pi/2)
+            end},invincible=true,highlight=true}
+            ::continue::
+        end
         local smallPortal=smallPortalSentry.any.portal
-        local spawner=BulletSpawner{lifeFrame=9999,period=DSWITCH{3,2,2,1},bulletNumber=1,angle=smallPortalAngle(),bulletSprite=BulletSprites.flame.red,bulletSpeed=150,highlight=true,bulletLifeFrame=300,bulletEvents={function (cir,args,self)
-            local phase=self.spawnTimes/20+self.spawnTimes%3*math.pi*2/3
-            cir.kinematicState.speed=cir.kinematicState.speed*(2-math.abs(math.cos(phase)))*math.eval(1,0.05*math.min(1,self.frame/1200))
-            local time=smallDistance*60/cir.kinematicState.speed*1.05
-            cir.kinematicState.dir=smallPortalAngle(time)
-            local offset=math.sin(phase)*smallLength*0.9*math.min(1,smallPortalSentry.any.ratio+time/60)
+        local ratioFunc=function ()
+            local t=bigPortalSentry.frame
+            return 0.2--0.05*math.cos(t/100)
+        end
+        local colors={'red','yellow','green','blue','purple','white','black'}
+        local groupN=15
+        local compensateFrame=30
+        local initPhase=math.eval(0,1)
+        local spawner=BulletSpawner{lifeFrame=9999,period=DSWITCH{4,3,2,1},bulletNumber=1,angle=smallPortalAngle,bulletSprite=BulletSprites.flame.red,bulletSpeed=350,highlight=true,bulletLifeFrame=600,bulletEvents={function (cir,args,self)
+            local groupIndex=self.spawnTimes%groupN
+            local phase=math.sin(initPhase+self.spawnTimes%3*math.pi*2/3+math.cos(self.spawnTimes/60))--%2-1
+            --cir.kinematicState.speed=cir.kinematicState.speed*math.eval(1,0.05*math.min(1,self.frame/1200))
+            local compensateDist=cir.kinematicState.speed*(groupIndex*self.period)/60
+            cir.speedRef=cir.kinematicState.speed
+            cir.kinematicState.speed=cir.kinematicState.speed+compensateDist*60/compensateFrame*2
+            cir.speed0=cir.kinematicState.speed
+            local offset=phase*smallLength*ratioFunc()
             cir.kinematicState.pos=geo:rThetaGo(cir.kinematicState.pos,offset,cir.kinematicState.dir+math.pi/2)
             cir.flashPeriod=math.eval(60,20)
         end},bulletExtraUpdate={Action.FadeOut(20,true),function (self)
             if self.kinematicState.teleportedPortals and self.frame>60 then
-                if Portal.objects[self.kinematicState.teleportedPortals[1]]==smallPortal then -- prevent secondary messy bullets
+                local portal=Portal.objects[self.kinematicState.teleportedPortals[1]]
+                if portal==bigPortal then -- prevent teleporting from reaching big portal's back side
                     self:remove()
+                end
+                if portal==smallPortal then
+                    self.teleportCount=(self.teleportCount or 0)+1
+                    self:changeSpriteColor(colors[self.teleportCount%#colors+1])
                 end
             end
             local phase=self.frame/self.flashPeriod*math.pi*2
-            local val=math.sin(phase)*0.4+0.6
+            local val=math.sin(phase)*0.2+0.8
             self.spriteColor={1,val,val,1}
+            self.kinematicState.speed=(self.speed0-self.speedRef)*(1-math.min(1,self.frame/compensateFrame))+self.speedRef-math.min((self.teleportCount or 0)*50,280)
         end}}
         spawner:bindState(boss)
-        Event.LoopEvent{obj=smallPortalSentry,period=1,executeFunc=function (self,times)
-            smallPortalSentry.kinematicState.dir=smallPortalAngle()+math.pi
-        end}
         bigPortal:link(smallPortal)
-        local function upward(sign,idx)
-            local period=22
-            local maxtan=3
-            local num=DSWITCH{3,3,3,3}
-            local type=math.random(1,DSWITCH{1,2,4,6})--idx%6+1--math.ceil(math.pseudoRandom(idx)*6)
-            local upwardSpawner=BulletSpawner{lifeFrame=period-1,period=1,bulletNumber=num,angle=dir0,bulletSprite=BulletSprites.kunai.blue,bulletSize=1.5,bulletSpeed=math.min(100,50+idx*10),highlight=true,bulletLifeFrame=300,bulletEvents={function (cir,args,self)
-                local time=self.frame%period
-                local ratio=time/period-0.5
-                local tan=ratio*maxtan
-                local index=args.index-num/2-0.5
-                if type==2 then
-                    tan=tan+index*0.1
-                elseif type==3 then
-                    tan=tan-index*0.1
-                elseif type>=4 then
-                    local gap=(type-3)*0.1
-                    local n=math.ceil(2/gap)
-                    local extra=gap*(n-1)
-                    tan=ratio*(maxtan-extra)+gap*math.floor(ratio*n)+index*gap/3
-                end
-                local angle=math.atan(tan)*sign
-                cir.kinematicState.dir=angle+dir0-math.pi/2
-                cir.kinematicState.speed=(cir.kinematicState.speed+10*args.index)/math.cos(angle)
-                cir.speedRef=cir.kinematicState.speed
-                cir.kinematicState.speed=cir.kinematicState.speed*(1+time/20)
-                cir.speed0=cir.kinematicState.speed
-                cir.flashPeriod=math.eval(60,20)
-                local val=1-math.abs(ratio)*1.6
-                cir.spriteColor={1,val,1,1}
-            end},bulletExtraUpdate={Action.FadeOut(20,true),function (self)
-                self.kinematicState.speed=math.lerp(self.speed0,self.speedRef,math.min(1,self.frame/40))
-            end}}
-            upwardSpawner:bindState(boss)
-        end
-        Event.LoopEvent{obj=smallPortalSentry,period=120,executeFunc=function (self,times)
-            upward(math.mod2Sign(times),times+2)
-        end}
     end
 }
